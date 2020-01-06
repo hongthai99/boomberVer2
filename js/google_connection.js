5af9422582d7e1e8d3e75e44db72c0045bea09a4
Bach Hong Thai <bachhongthai@Hong-Thai-Bach.local>
2020-01-06 16:29:16 +0700

    update 6/1/2020

--- js/google_connection.js
+++ js/google_connection.js
@@ -26,3 +26,68 @@ function insert_value()
     });
     console.log(request);
   }
+
+  function read_value() {
+
+    var url = script_url + "?action=read";
+  
+    $.getJSON(url, function(json) {
+  
+      // Set the variables from the results array
+  
+      // CREATE DYNAMIC TABLE.
+      var table = document.createElement("table");
+  
+  
+  
+      var header = table.createTHead();
+      //Es el correspondiente a una columna en la tabla
+      var row = header.insertRow(0);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+    
+  
+      // Es el Titulo de cada columna
+      cell1.innerHTML = "<b>Create_date</b>";
+      cell2.innerHTML = "<b>Phone</b>";
+      cell3.innerHTML = "<b>Name</b>";
+      cell4.innerHTML = "<b>Address</b>";
+      cell5.innerHTML = "<b>Description</b>";
+  
+      // ADD JSON DATA TO THE TABLE AS ROWS
+      //LOS NOMBRES DEBEN CORRESPONDER AL NOMBRE DE LAS COLUMNAS
+      //DEL SHEET
+    for (var i =0; i < json.records.length; i++)
+      {
+        tr = table.insertRow(-1);
+  
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = json.records[i].Create_date;
+  
+        tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = json.records[i].Phone;
+  
+        tabCell = tr.insertCell(-1);
+        console.log(json);
+        tabCell.innerHTML = json.records[i].Name;
+  
+        tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = json.records[i].Address;
+  
+        tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = json.records[i].Description;
+  
+  
+      }
+  
+  
+      // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+      var divContainer = document.getElementById("showData");
+      divContainer.innerHTML = "";
+      divContainer.appendChild(table);
+    });
+  }
+  
