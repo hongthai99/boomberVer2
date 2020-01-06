d581023a61a840e63358bcf5a152ba431fb93477
Bach Hong Thai <bachhongthai@Hong-Thai-Bach.local>
2020-01-06 22:54:33 +0700

    update 6/1/2020 11h

--- js/google_connection.js
+++ js/google_connection.js
@@ -13,7 +13,7 @@ function insert_value()
     var row5 = $("#Description").val();
   
     var url = script_url + "?callback=ctrlq&Create_date"+ row1 +"&Phone=" + row2 + "&Name="+ row3 + 
-     +"&Address=" + row4 + "&Description="+ row5 +"&action=insert";
+    "&Address=" + row4 + "&Description="+ row5 +"&action=insert";
     console.log(url);
   
   
@@ -25,69 +25,68 @@ function insert_value()
       dataType: "jsonp"
     });
     console.log(request);
-  }
+}
 
-  function read_value() {
 
-    var url = script_url + "?action=read";
+// readdata from here
+function read_value() {
+
+  $("#re").css("visibility", "hidden");
+
+  document.getElementById("loader").style.visibility = "visible";
+  var url = script_url + "?action=read";
+
+  $.getJSON(url, function(json) {
+
+    // Set the variables from the results array
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+
+
+
+    var header = table.createTHead();
+    //Es el correspondiente a una columna en la tabla
+    var row = header.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
   
-    $.getJSON(url, function(json) {
-  
-      // Set the variables from the results array
-  
-      // CREATE DYNAMIC TABLE.
-      var table = document.createElement("table");
-  
-  
-  
-      var header = table.createTHead();
-      //Es el correspondiente a una columna en la tabla
-      var row = header.insertRow(0);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      var cell4 = row.insertCell(3);
-      var cell5 = row.insertCell(4);
-    
-  
-      // Es el Titulo de cada columna
-      cell1.innerHTML = "<b>Create_date</b>";
-      cell2.innerHTML = "<b>Phone</b>";
-      cell3.innerHTML = "<b>Name</b>";
-      cell4.innerHTML = "<b>Address</b>";
-      cell5.innerHTML = "<b>Description</b>";
-  
-      // ADD JSON DATA TO THE TABLE AS ROWS
-      //LOS NOMBRES DEBEN CORRESPONDER AL NOMBRE DE LAS COLUMNAS
-      //DEL SHEET
-    for (var i =0; i < json.records.length; i++)
-      {
-        tr = table.insertRow(-1);
-  
-        var tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = json.records[i].Create_date;
-  
-        tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = json.records[i].Phone;
-  
-        tabCell = tr.insertCell(-1);
-        console.log(json);
-        tabCell.innerHTML = json.records[i].Name;
-  
-        tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = json.records[i].Address;
-  
-        tabCell = tr.insertCell(-1);
-        tabCell.innerHTML = json.records[i].Description;
-  
-  
-      }
-  
-  
-      // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-      var divContainer = document.getElementById("showData");
-      divContainer.innerHTML = "";
-      divContainer.appendChild(table);
-    });
-  }
-  
+
+    // ADD JSON DATA TO THE TABLE AS ROWS
+    //LOS NOMBRES DEBEN CORRESPONDER AL NOMBRE DE LAS COLUMNAS
+    //DEL SHEET
+  for (var i =0; i < json.records.length; i++)
+    {
+      tr = table.insertRow(-1);
+
+      var tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = json.records[i].Create_date;
+
+      tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = json.records[i].Phone;
+
+      tabCell = tr.insertCell(-1);
+      console.log(json);
+      tabCell.innerHTML = json.records[i].Name;
+
+      tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = json.records[i].Address;
+
+      tabCell = tr.insertCell(-1);
+      tabCell.innerHTML = json.records[i].Description;
+
+
+    }
+
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+    document.getElementById("loader").style.visibility = "hidden";
+    $("#re").css("visibility", "visible");
+  });
+}
